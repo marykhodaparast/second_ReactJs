bae8044bdd797b531eddf01b0344350ce684c220
“webbakery-maryam” <maryam@web-bakery.de>
2024-07-03 13:39:32 +0330

    working on different styles completed

---
+++ vite-project/src/Button.jsx
@@ -0,0 +1,14 @@
+
+
+function Button() {
+  const styles = {
+    backgroundColor: "hsl(200, 100%, 50%)",
+    color:"white",
+    padding: "20px 10px",
+    borderRadius:"5px",
+    border:"none",
+    cursor:"pointer"
+  }
+  return <button style={styles}>Click me!</button>;
+}
+export default Button;
