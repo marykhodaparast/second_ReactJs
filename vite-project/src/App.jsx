b76c5c0e658d5985981991eb06adabcb6a59c096
“webbakery-maryam” <maryam@web-bakery.de>
2024-07-03 13:32:29 +0330

    work with module.css

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,10 @@
-import Card from "./Card.jsx";
+import Button from "./Button/Button.jsx";
 
 function App() {
   return (
     <>
-      <Card />
-      <Card />
-      <Card />
+      <Button />
+      
     </>
   );
 }
