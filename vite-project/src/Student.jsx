bd0cddcd1bb9ce7c317ca7e8fed0a7d23e3b8123
“webbakery-maryam” <maryam@web-bakery.de>
2024-07-06 12:59:15 +0330

    props learning completed

--- vite-project/src/Student.jsx
+++ vite-project/src/Student.jsx
@@ -1,10 +1,23 @@
 /* eslint-disable react/prop-types */
+import PropTypes from 'prop-types';
 function Student(props) {
    return (
-    <div>
-        <p>Name: {props.name}</p>
+     <div className="student">
+       <p>Name: {props.name}</p>
+       <p>Age: {props.age}</p>
+       <p>Student: {props.isStudent ? 'Yes' : 'No'}</p>
+     </div>
+   );
+}
+Student.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  isStudent: PropTypes.bool
 
-    </div>
-   )
+}
+Student.defaultProps = {
+    name: "Guest",
+    age: 0,
+    isStudent: false 
 }
 export default Student;
