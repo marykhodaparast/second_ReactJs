42b189c667d25ff2816cee2fcd885245dfc7c708
“webbakery-maryam” <maryam@web-bakery.de>
2024-07-06 15:41:44 +0330

    work on List Part 1

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,25 @@
-import UserGreeting from "./UserGreeting.jsx";
+import List from "./List.jsx";
 
 function App() {
+  const fruits = [
+    { id: 1, name: "apple", calaries: 95 },
+    { id: 2, name: "orange", calaries: 45 },
+    { id: 3, name: "banana", calaries: 105 },
+    { id: 4, name: "coconut", calaries: 159 },
+    { id: 5, name: "pineapple", calaries: 37 },
+  ];
+
+  const vegetables = [
+    { id: 6, name: "potatoes", calaries: 110 },
+    { id: 7, name: "celery", calaries: 15 },
+    { id: 8, name: "carrots", calaries: 25 },
+    { id: 9, name: "corn", calaries: 63 },
+    { id: 10, name: "brocolli", calaries: 50 },
+  ];
   return (
     <>
-      <UserGreeting isLoggedIn={true} username="Maryam"/>
-      <UserGreeting/>
+      <List items={fruits} category="Fruits" />
+      <List items={vegetables} category="Vegetables" />
     </>
   );
 }
---
+++ vite-project/src/List.jsx
@@ -0,0 +1,33 @@
+function List(props) {
+//   const fruits = [
+//     { id: 1, name: "apple", calaries: 95 },
+//     { id: 2, name: "orange", calaries: 45 },
+//     { id: 3, name: "banana", calaries: 105 },
+//     { id: 4, name: "coconut", calaries: 159 },
+//     { id: 5, name: "pineapple", calaries: 37 },
+//   ];
+
+  //fruits.sort((a,b) => a.name.localeCompare(b.name));ALPHABETICAL
+  //fruits.sort((a,b) => b.name.localeCompare(a.name));REVERSE ALPHABETICAL
+  //fruits.sort((a,b) => a.calaries - b.calaries);NUMERIC ORDER
+  //fruits.sort((a, b) => b.calaries - a.calaries); //REVERSE NUMERIC ORDER
+
+  //const lowCalFruits = fruits.filter(fruit => fruit.calaries < 100)
+  //const highCalFruits = fruits.filter(fruit => fruit.calaries >= 100)
+
+  const category = props.category;
+  const itemList = props.items;
+  const listItems = itemList.map((item) => (
+    <li key={item.id}>
+      {item.name}: &nbsp; <b>{item.calaries}</b>
+    </li>
+  ));
+
+  return (
+    <>
+      <h3 className="list-category">{category}</h3>
+      <ol className="list-items">{listItems}</ol>
+    </>
+  );
+}
+export default List;
