411796bb04c11578cb6438db746d6270223a2168
“webbakery-maryam” <maryam@web-bakery.de>
2024-07-31 15:16:18 +0330

    useEffect for height and width completed

--- vite-project/src/MyComponent.jsx
+++ vite-project/src/MyComponent.jsx
@@ -1,36 +1,32 @@
 import React, { useState, useEffect } from "react";
 
 function MyComponent() {
-  const [count, setCount] = useState(0);
-  const [color, setColor] = useState("green");
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
 
   useEffect(() => {
-    document.title = `Count: ${count} ${color}`;
+    window.addEventListener("resize", handleResize);
+    console.log("EVENT LISTENER ADDED.");
 
     return () => {
-        //SOME CLEANUP CODE
+      window.removeEventListener('resize', handleResize);
+      console.log("EVENT LISTENER REMOVED");
     }
+  }, []);
 
-  }, [count, color]);
+  useEffect(() => {
+    document.title = `Size: ${width} x ${height}`;
+  }, [width, height]);
 
-  function addCount() {
-    setCount(c => c + 1);
-  }
-
-  function subtractCount() {
-    setCount((c) => c - 1);
-  }
-
-  function changeColor() {
-    setColor(c => c === "green" ? "red" : "green");
+  function handleResize() {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
   }
 
   return (
     <>
-      <p style={{color: color}}>Count: {count}</p>
-      <button onClick={addCount}>Add</button>
-      <button onClick={subtractCount}>Subtract</button>
-      <button onClick={changeColor}>Change Color</button>
+      <p>Window width: {width}px</p>
+      <p>Window Height: {height}px</p>
     </>
   );
 }
